c593dee76642de6b4a9871c24544a314a30fd8a9
kartikb-tailscale <kbpersonal@killinit.cc>
2025-07-04 10:53:56 -0400

    AWS lab env pops

---
+++ terraform/components/terraform/pops-aws/outputs.tf
@@ -0,0 +1,13 @@
+output "Message" {
+  description = "Instructions for configuring your environment after Terraform apply."
+  value = join("\n", compact([
+    "Next Steps:",
+    "1. Test SSH to each EC2 instance's public IP (Only available if private APIServer endpoint is enabled):",
+    local.enable_sr && length(data.aws_instances.sr_ec2.public_ips) > 0 ? join("\n", [
+      for idx, ip in data.aws_instances.sr_ec2.public_ips :
+        "   ssh -i ~/.ssh/${local.key_name} ubuntu@${ip} # ${local.sr_instance_hostname}-${idx + 1}"
+    ]) : "   N/A",
+    "",
+    "Happy deploying <3"
+  ]))
+}
