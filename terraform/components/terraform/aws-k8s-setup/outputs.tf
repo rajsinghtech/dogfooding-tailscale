f8bdf1d4d9edcbc06e14820d966ab5ff3e05e797
kartikb-tailscale <kbpersonal@killinit.cc>
2026-01-28 16:59:11 -0500

    fix outputs

--- terraform/components/terraform/aws-k8s-setup/outputs.tf
+++ terraform/components/terraform/aws-k8s-setup/outputs.tf
@@ -3,7 +3,7 @@ output "Message" {
   value       = <<-EOT
 Next Steps:
 1. Configure your kubeconfig for direct APIserver access by running:
-   aws eks --region ${local.region} update-kubeconfig --name ${local.name} --alias ${local.name}
+   aws eks --region ${local.region} update-kubeconfig --name ${local.cluster_name} --alias ${local.cluster_name}
 
 2. Configure your kubeconfig for Tailscale Operator APIserver proxy access by running:
    tailscale configure kubeconfig ${local.tenant}-${local.environment}-${local.stage}-operator
